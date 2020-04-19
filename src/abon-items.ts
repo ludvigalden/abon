566949126f5ebae0198ffdc1e8e1e325afd037fb
ludvigalden <aldenludvig@gmail.com>
2020-04-19 22:44:49 +0200

    AbonItems comments and new methods

--- src/abon-items.ts
+++ src/abon-items.ts
@@ -6,7 +6,23 @@ import { ChangeListener, UnsubscribeFn, ItemRecord, ItemRecordKey, ItemsChangeLi
 import { Abon } from "./abon";
 
 export class AbonItems<T extends object, I extends keyof T> extends AbonDeep<ItemRecord<T, I>>
-    implements Pick<Array<T>, "find" | "findIndex" | "pop" | "values"> {
+    implements
+        Pick<
+            Array<T>,
+            | "find"
+            | "findIndex"
+            | "pop"
+            | "values"
+            | "forEach"
+            | "map"
+            | "indexOf"
+            | "lastIndexOf"
+            | "every"
+            | "some"
+            | "filter"
+            | "includes"
+            | "length"
+        > {
     ids: Abon<T[I][]>;
 
     constructor(readonly idKey: I, initial?: T[]) {
@@ -15,9 +31,13 @@ export class AbonItems<T extends object, I extends keyof T> extends AbonDeep<Ite
         this.ids = new Abon(AbonItems.ids(initial || [], idKey));
     }
 
+    /** Set the items */
     set(items: T[]): this;
+    /** Update existing items */
     set(current: ItemRecord<T, I>): this;
+    /** Update an existing item */
     set(id: T[I], item: T): this;
+    /** Set a value of an existing item */
     set<K extends keyof T>(id: T[I], key: K, value: T[K]): this;
     set(...args: any[]): this {
         const { keys, value } = AbonDeep.parseKeyValueArgs(args);
@@ -79,10 +99,22 @@ export class AbonItems<T extends object, I extends keyof T> extends AbonDeep<Ite
         return this;
     }
 
-    delete(...ids: T[I][]) {
-        return this.set(this.array.filter((item) => !item || !ids.includes(item[this.idKey])));
+    /** Deletes items from the list and ensures the right order of remaining items */
+    delete(...ids: T[I][]): this;
+    delete(...items: T[]): this;
+    delete(...idsOrItems: (T | T[I])[]): this;
+    delete(...idsOrItems: (T | T[I])[]) {
+        const ids = idsOrItems
+            .filter((idOrItem) => idOrItem != null)
+            .map((idOrItem) => (typeof idOrItem === "object" ? (idOrItem as T)[this.idKey] : idOrItem));
+
+        return this.set(this.filter((item) => !item || !ids.includes(item[this.idKey])));
     }
 
+    /**
+     * Appends new items to the end of the list and returns the new amonut of items.
+     * @param items New items to append.
+     */
     push(...items: T[]): number {
         if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test") {
             const undefinedItemIndex = this.array.findIndex((item) => !item || item[this.idKey] == null);
@@ -115,6 +147,10 @@ export class AbonItems<T extends object, I extends keyof T> extends AbonDeep<Ite
         return this.length;
     }
 
+    /**
+     * Inserts new items at the start of the list.
+     * @param items  New items to insert at the start of the list.
+     */
     unshift(...items: T[]): number {
         if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test") {
             const undefinedItemIndex = this.array.findIndex((item) => !item || item[this.idKey] == null);
@@ -146,8 +182,153 @@ export class AbonItems<T extends object, I extends keyof T> extends AbonDeep<Ite
         return this.length;
     }
 
-    has(id: T[I]) {
-        return Boolean(this.current[id as ItemRecordKey<T, I>]);
+    /**
+     * Returns the value of the first element in the array where predicate is true, and undefined
+     * otherwise.
+     * @param predicate find calls predicate once for each element of the array, in ascending
+     * order, until it finds one where predicate returns true. If such an element is found, find
+     * immediately returns that element value. Otherwise, find returns undefined.
+     */
+    find<S extends T>(predicate: (this: void, value: T, index: number, obj: T[]) => value is S): S | undefined;
+    find(predicate: (value: T, index: number, obj: T[]) => unknown): T | undefined;
+    find(...args: any[]): any {
+        return this.array.find(...(args as [any]));
+    }
+
+    /**
+     * Determines whether an array includes a certain element, returning true or false as appropriate.
+     * @param searchElement The element to search for.
+     */
+    includes(item: T): boolean;
+    includes(id: T[I]): boolean;
+    includes(thunk: T | T[I]): boolean {
+        return this.indexOf(thunk) > 0;
+    }
+
+    /**
+     * Returns the index of the first element in the array where predicate is true, and -1
+     * otherwise.
+     * @param predicate find calls predicate once for each element of the array, in ascending
+     * order, until it finds one where predicate returns true. If such an element is found,
+     * findIndex immediately returns that element index. Otherwise, findIndex returns -1.
+     */
+    findIndex(predicate: (value: T, index: number, obj: T[]) => unknown): number {
+        return this.array.findIndex(predicate);
+    }
+
+    /**
+     * Returns the index of the first occurrence of a value in an array.
+     * @param searchElement The value to locate in the array.
+     * @param fromIndex The array index at which to begin the search. If fromIndex is omitted, the search starts at index 0.
+     */
+    indexOf(item: T, fromIndex?: number): number;
+    indexOf(id: T[I], fromIndex?: number): number;
+    indexOf(thunk: T | T[I], fromIndex?: number): number;
+    indexOf(thunk: T | T[I], fromIndex?: number): number {
+        const id = thunk && typeof thunk === "object" ? (thunk as T)[this.idKey] : thunk;
+        return this.ids.current.indexOf(id, fromIndex);
+    }
+
+    /**
+     * Returns the index of the last occurrence of a specified value in an array.
+     * @param searchElement The value to locate in the array.
+     * @param fromIndex The array index at which to begin the search. If fromIndex is omitted, the search starts at the last index in the array.
+     */
+    lastIndexOf(item: T, fromIndex?: number): number;
+    lastIndexOf(id: T[I], fromIndex?: number): number;
+    lastIndexOf(thunk: T | T[I], fromIndex?: number): number;
+    lastIndexOf(thunk: T | T[I], fromIndex?: number): number {
+        const id = thunk && typeof thunk === "object" ? (thunk as T)[this.idKey] : thunk;
+        return this.array.lastIndexOf(id as any, fromIndex);
+    }
+
+    /**
+     * Determines whether all the members of an array satisfy the specified test.
+     * @param callbackfn A function that accepts up to three arguments. The every method calls
+     * the callbackfn function for each element in the array until the callbackfn returns a value
+     * which is coercible to the Boolean value false, or until the end of the array.
+     */
+    every(callbackfn: (value: T, index: number, array: T[]) => unknown): boolean {
+        return this.array.every(callbackfn);
+    }
+
+    /**
+     * Determines whether the specified callback function returns true for any element of an array.
+     * @param callbackfn A function that accepts up to three arguments. The some method calls
+     * the callbackfn function for each element in the array until the callbackfn returns a value
+     * which is coercible to the Boolean value true, or until the end of the array.
+     */
+    some(callbackfn: (value: T, index: number, array: T[]) => unknown): boolean {
+        return this.array.some(callbackfn);
+    }
+
+    /**
+     * Performs the specified action for each element in an array.
+     * @param callbackfn  A function that accepts up to three arguments. forEach calls the callbackfn function one time for each element in the array.
+     */
+    forEach(callbackfn: (value: T, index: number, array: T[]) => void): void {
+        this.array.forEach(callbackfn);
+    }
+
+    /**
+     * Calls a defined callback function on each element of an array, and returns an array that contains the results.
+     * @param callbackfn A function that accepts up to three arguments. The map method calls the callbackfn function one time for each element in the array.
+     */
+    map<U>(callbackfn: (value: T, index: number, array: T[]) => U): U[] {
+        return this.array.map(callbackfn);
+    }
+
+    /**
+     * Returns the elements of an array that meet the condition specified in a callback function.
+     * @param callbackfn A function that accepts up to three arguments. The filter method calls the callbackfn function one time for each element in the array.
+     * @param thisArg An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
+     */
+    filter<S extends T>(callbackfn: (value: T, index: number, array: T[]) => value is S): S[];
+    /**
+     * Returns the elements of an array that meet the condition specified in a callback function.
+     * @param callbackfn A function that accepts up to three arguments. The filter method calls the callbackfn function one time for each element in the array.
+     * @param thisArg An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
+     */
+    filter(callbackfn: (value: T, index: number, array: T[]) => unknown): T[];
+    filter<S extends T>(callbackfn: (value: T, index: number, array: T[]) => value is S): S[] {
+        return this.array.filter(callbackfn);
+    }
+
+    /** Removes the last item and returns it. */
+    pop(): T | undefined {
+        const pop = this.ids.current[this.ids.current.length - 1];
+
+        if (pop != null) {
+            const value = this.get(pop as ItemRecordKey<T, I>);
+            this.delete(pop);
+            return value;
+        } else {
+            return undefined;
+        }
+    }
+
+    /** Reverse the items */
+    reverse(): this {
+        this.set(this.array.reverse());
+        return this;
+    }
+
+    /** Returns an iterable of the items */
+    values(): IterableIterator<T> {
+        return this.array.values();
+    }
+
+    [Symbol.iterator](): IterableIterator<T> {
+        return this.array[Symbol.iterator]();
+    }
+
+    /** The values of the set items in the same order as the `ids` */
+    get array() {
+        return this.ids.current.map((id): T => this.current[id as ItemRecordKey<T, I>]);
+    }
+
+    get length() {
+        return this.ids.current.length;
     }
 
     subscribe(listener: ItemsChangeListener<T, I>): UnsubscribeFn;
@@ -253,52 +434,6 @@ export class AbonItems<T extends object, I extends keyof T> extends AbonDeep<Ite
         }
 
         return this;
-    }
-
-    find<S extends T>(predicate: (this: void, value: T, index: number, obj: T[]) => value is S, thisArg?: any): S | undefined;
-    find(predicate: (value: T, index: number, obj: T[]) => unknown, thisArg?: any): T | undefined;
-    find(...args: any[]): any {
-        return this.array.find(...(args as [any]));
-    }
-
-    findIndex(predicate: (value: T, index: number, obj: T[]) => unknown, thisArg?: any): number;
-    findIndex(...args: any[]): any {
-        return this.array.findIndex(...(args as [any]));
-    }
-
-    /** Remove the last item */
-    pop(): T | undefined {
-        const pop = this.ids.current[this.ids.current.length - 1];
-
-        if (pop != null) {
-            const value = this.get(pop as ItemRecordKey<T, I>);
-            this.delete(pop);
-            return value;
-        } else {
-            return undefined;
-        }
-    }
-
-    /** Reverse the items */
-    reverse(): this {
-        this.set(this.array.reverse());
-        return this;
-    }
-
-    values(): IterableIterator<T> {
-        return this.array.values();
-    }
-
-    [Symbol.iterator](): IterableIterator<T> {
-        return this.array[Symbol.iterator]();
-    }
-
-    get array() {
-        return this.ids.current.map((id): T => this.current[id as ItemRecordKey<T, I>]);
-    }
-
-    get length() {
-        return this.ids.current.length;
     }
 
     static record<T extends object, I extends keyof T>(items: T[], idKey: I) {
